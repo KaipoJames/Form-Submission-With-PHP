72a97934ec5c2fe93793ec799f2ffe2ece79fd7d
Kaipo Wilmeth <kaipojames12@gmail.com>
2020-09-17 18:38:57 -1000

    Modify users.php

--- users.php
+++ users.php
@@ -11,8 +11,44 @@
     <?php
     include('header.php');
 
-    include('functions.php');
-    displayDatabaseUsers();
+    function displayDatabaseUsers()
+    {
+        $db = connect_to_DB();
+        $display_users_query = "SELECT * FROM users";
+        $results = mysqli_query($db, $display_users_query);
+        //print $results . "</br>";
+        if (!$results) {
+            print("</br>Error Description: " . mysqli_error($db));
+            exit();
+            //$valid_post = false;
+            //$display_users_error = true;
+        }
+    ?>
+    <p>There are <?php echo mysqli_num_rows($results) ?> members.</p>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Hashed Password</th>
+        </tr>
+        <?php
+            while ($row = $results->fetch_assoc()) { ?>
+        <tr>
+            <td><?php echo $row["id"]; ?></td>
+            <td><?php echo $row["name"]; ?></td>
+            <td>
+                <a href="profile.php?id=<?php echo $row["id"]; ?>">
+                    <?php echo $row["email"]; ?></a>
+            </td>
+            <td><?php echo $row["hashed_password"]; ?></td>
+        </tr>
+        <?php
+            }
+            ?>
+    </table>
+    <?php
+    }
 
 
     include('footer.php');
